e24dfff55de7e3d77d3eff8e11a9e1c20ab58db5
Your Name <you@example.com>
2020-12-18 14:58:03 +0300

    Init

---
+++ src/com/javacourse/se/lesson5/Main.java
@@ -0,0 +1,10 @@
+package com.javacourse.se.lesson5;
+
+public class Main {
+    public static void main(String []args) {
+        System.out.println("Осень ушла;");
+        System.out.println("Чрез горе встретились мы;");
+        System.out.println("Лист оборвался.");
+        System.out.println("Daunnc©");
+    }
+}
